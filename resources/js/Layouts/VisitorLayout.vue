480d508e6debaa89152ce625177449b770160d5e
TheoRhytical <rhyscabonita@gmail.com>
2024-01-09 10:17:33 +0800

    Feat: visitor layout

---
+++ resources/js/Layouts/VisitorLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import IndexNav from '@/Components/IndexNav.vue';
+
+
+</script>
+
+<template>
+<main class="min-h-screen">
+	<IndexNav />
+	<slot />
+</main>
+</template>
+
+<style scoped>
+main {
+  background: linear-gradient(45deg, #eeb405 10%, #dc3545 100%);
+  font-size: 16px;
+  font-family: Tahoma;
+}
+</style>
