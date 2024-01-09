35858a65943536b1067ee4d6941a81baed2bec45
TheoRhytical <rhyscabonita@gmail.com>
2024-01-09 10:09:58 +0800

    Register customer API

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
+use Intervention\Image\ImageManager;
 
 class RegisteredUserController extends Controller
 {
@@ -29,24 +35,77 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): JsonResponse
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'full_name' => ['required', 'string', 'max:255', 'unique:'.Customer::class],
+            'date_of_birth' => ['required', 'date'],
+            'address' => ['required', 'string', 'max:'. Customer::$addressMaxLength],
+            'email' => ['required', 'email:rfc,dns', 'unique:'.User::class],
+            'phone_number' => ['required', 'string', 'regex:/^\+63-(9\d{2})-\d{3}-\d{4}$/', 'unique:'.Customer::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'signature' => ['required', 'image'],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
 
-        event(new Registered($user));
+        try {
+			DB::beginTransaction();
+            // Create new customer instance without saving (to use id later)
+            $user = User::create([
+                'email' => $request->email,
+                'full_name' => $request->full_name,
+                'user_type' => 'customer',
+                'password' => Hash::make($request->password),
+            ]);
+        
+            $customer = Customer::create([
+                'full_name' => $request->full_name,
+                'date_of_birth' => $request->date_of_birth,
+                'address' => $request->address,
+                'phone_number' => $request->phone_number,
+                'card_status' => 'processing',
+                'user_id' => $user->id,
+            ]);
+
+            $image = $request->signature;
+
+            // Resize signature file dimensions
+            $resizedSignatureImage = ImageManager::gd()
+                ->read($image)
+                ->scaleDown(400, 400);
+
+            $filename = "{$customer->id}-signature.{$image->getClientOriginalExtension()}";
+        
+            $resizedSignatureImage->save(storage_path("app/customer-signatures/{$filename}"));
+            $customer->update(['signature_filename' => $filename]);
+            DB::commit();
+        } catch (\Exception|\Throwable $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            abort(HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        event(new Registered($customer));
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return response()->json([
+            'message' => 'Congratulations! Your Prince Card registration is complete.',
+        ], 201);
+
+
+
+
+        // $user = User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request->password),
+        // ]);
+
+        // event(new Registered($user));
+
+        // Auth::login($user);
+
+        // return redirect(RouteServiceProvider::HOME);
     }
 }
