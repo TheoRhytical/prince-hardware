f01fc243382ffc8b4c7fa0a91334c095ab13b4f7
TheoRhytical <rhyscabonita@gmail.com>
2024-01-09 10:14:55 +0800

    Phone number input mask feat

---
+++ resources/js/Components/PhoneNumInputMask.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { vMaska } from "maska"
+
+const props = defineProps({
+	'modelValue': {
+		type: String
+	},
+	'inputClass': {
+		type: String
+	},
+	'required': {
+		type: Boolean
+	}
+})
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+<div>
+	<input
+		:required="required"
+		type="tel"
+		:class="[inputClass]"
+
+    :value="props.modelValue"
+    @input="emit('update:modelValue', $event.target.value)"
+
+		v-maska
+		data-maska="+63-9##-###-####"
+		placeholder="+63-9__-___-____"
+		style="margin-bottom: 0;"
+	/>
+	<p>Ex: +63-912-345-6789</p>
+</div>
+</template>
