52a8375e0d5b0146af3a28c5707db2fc20bfcf5f
TheoRhytical <rhyscabonita@gmail.com>
2024-01-10 07:56:24 +0800

    Transferred delete modal to component

---
+++ resources/js/Components/DeleteRecordModal.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+import Modal from './Modal.vue';
+
+const props = defineProps({
+	toDeleteId: {
+		// type: Number,
+		required: true,
+	},
+})
+
+const emits = defineEmits(['deleted'])
+
+const deleteModalVisible = ref(false)
+const deleteCustomerSuccessMessage = ref('')
+const deleteSuccessModalVisible = ref(false)
+const submitting = ref(false)
+
+watchEffect(() => {
+	if (props.toDeleteId !== null) deleteModalVisible.value = true
+})
+
+const deleteCustomer = () => {
+	submitting.value = true
+	axios.delete(route('customer.delete'), {
+		data: {
+			id: toDeleteId
+		}
+	})
+	.then(res => {
+		console.log('success', res)
+		deleteCustomerSuccessMessage.value = res.data.message
+		deleteSuccessModalVisible.value = true
+	})
+	.catch(err => {
+		console.log('error', err)
+	})
+	.finally(() => {
+		submitting.value = false
+		deleteModalVisible.value = false
+	})
+}
+</script>
+
+<template>
+  <Modal
+		:show="deleteModalVisible" 
+		max-width='md' 
+		@close="deleteModalVisible = false"
+		modal-class="edit-modal"
+	>
+		<div class="modal-content" style="max-width: none;">
+			<span class="close" id="closeDeleteModal" @click="deleteModalVisible = false">&times;</span>
+			<p>Are you sure you want to delete this record?</p>
+			<div class="flex justify-center">
+				<button 
+					:disabled="submitting"
+					id="confirmDelete" 
+					@click="deleteCustomer"
+				>
+					YES
+				</button>
+				<button id="cancelDelete" @click="deleteModalVisible = false">NO</button>
+			</div>
+		</div>
+	</Modal>
+
+  <Modal
+		:show="deleteSuccessModalVisible" 
+		max-width='md' 
+		@close="deleteSuccessModalVisible = false"
+		modal-class="edit-modal"
+	>
+		<div class="modal-content" style="max-width: none;">
+			<span @click="deleteSuccessModalVisible = false" class="close" id="closeUpdateModal">&times;</span>
+			{{ deleteCustomerSuccessMessage}}
+		</div>
+	</Modal>
+</template>
